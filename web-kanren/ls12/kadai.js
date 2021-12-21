eb3f0a838c9927fd66bf86ebc4a497f64ba22e81
Kouta-fd <kotafd0124@icloud.com>
2021-12-21 15:46:30 +0900

    add:kadai

--- web-kanren/ls12/kadai.js
+++ web-kanren/ls12/kadai.js
@@ -1,18 +1,35 @@
 window.onload = function () {
   var uploadArea = document.getElementById("area");
-  uploadArea.addEventListener("dragover", (event) => {
+  uploadArea.addEventListener("dragover", function (event) {
     event.preventDefault();
     event.target.classList.add("drag");
   });
-  uploadArea.addEventListener("dragleave", (event) => {
+  uploadArea.addEventListener("dragleave", function (event) {
     event.target.classList.remove("drag");
   });
   // 追加のdropイベント
-  uploadArea.addEventListener("drop", (event) => {
+  uploadArea.addEventListener("drop", function (event) {
     event.preventDefault();
-    var input = document.querySelectorAll('input[name="fileupload"]'); //好きなようにDOMを取得してください
-    input[0].files = event.dataTransfer.files; //取得したinput[type=file]にDropしたファイルを突っ込む
-    var file = document.getElementById("file").file[0];
-    this.alert(file.size)
+    var input = document.getElementById("file");
+    input.files = event.dataTransfer.files; //取得したinputにDropしたファイルを突っ込む
   });
+  //メモがローカルストレージにあるかを判断
+  var strage = "";
+  if (localStorage.getItem("strage") == null) {
+    strage = "メモはありません。";
+  } else {
+    strage = localStorage.getItem("strage");
+  }
+  document.getElementById("memo").value = strage;
 };
+var memo = document.getElementById("memo");
+//保存ボタン
+document.getElementById("save").addEventListener("click", function (event) {
+  var strage = memo.value;
+  localStorage.setItem("strage", strage);
+});
+//削除ボタン
+document.getElementById("clear").addEventListener("click", function (event) {
+  localStorage.clear();
+  memo.value = "";
+});
